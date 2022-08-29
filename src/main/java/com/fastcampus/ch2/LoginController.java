d307bc6a42abcc8f01cc72a30659ece42c9f6276
Jinhyeok <gjwlsgur123@gmail.com>
2022-08-29 16:04:28 +0900

    spring

--- src/main/java/com/fastcampus/ch2/LoginController.java
+++ src/main/java/com/fastcampus/ch2/LoginController.java
@@ -20,7 +20,7 @@ public class LoginController {
 	public String loginForm() {
 		return "loginForm";
 	}
-
+	
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		// 1. 세션을 종료
@@ -30,14 +30,14 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String login(String id, String pwd, String toURL, boolean rememberId,
-						HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public String login(String id, String pwd, String toURL, boolean rememberId, 
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		// 1. id와 pwd를 확인
 		if(!loginCheck(id, pwd)) {
 			// 2-1   일치하지 않으면, loginForm으로 이동
 			String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
-
+			
 			return "redirect:/login/login?msg="+msg;
 		}
 		// 2-2. id와 pwd가 일치하면,
@@ -45,9 +45,9 @@ public class LoginController {
 		HttpSession session = request.getSession();
 		//  세션 객체에 id를 저장
 		session.setAttribute("id", id);
-
+		
 		if(rememberId) {
-			//     1. 쿠키를 생성
+		//     1. 쿠키를 생성
 			Cookie cookie = new Cookie("id", id); // ctrl+shift+o 자동 import
 //		       2. 응답에 저장
 			response.addCookie(cookie);
@@ -60,7 +60,7 @@ public class LoginController {
 		}
 //		       3. 홈으로 이동
 		toURL = toURL==null || toURL.equals("") ? "/" : toURL;
-
+		
 		return "redirect:"+toURL;
 	}
 
